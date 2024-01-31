e3fa339473558f2e5dddfe369be700583297c786
JS-Project96 <jshorter206854@gmail.com>
2024-01-31 09:58:34 +0000

    added hint message when passwords do not match

--- js/script.js
+++ js/script.js
@@ -1,14 +1,17 @@
 let pwd1 = document.querySelector('.pwd1');
 let pwd2 = document.querySelector('.pwd2');
+let hint = document.querySelector('.hint');
 
 function checkPassword() {
     if (pwd1.value != pwd2.value) {
         pwd1.style.borderColor = 'red';
         pwd2.style.borderColor = 'red';
+        hint.style.display = 'block';
         return false;
     } else {
         pwd1.style.borderColor = 'green';
         pwd2.style.borderColor = 'green';
+        hint.style.display = 'none';
         return true;
     }
 };
