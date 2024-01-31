a1d6d50f90a45559302d400fff7c30f1971b6f73
JS-Project96 <jshorter206854@gmail.com>
2024-01-31 10:02:51 +0000

    added dynamic password feedback to user

--- js/script.js
+++ js/script.js
@@ -1,17 +1,20 @@
 let pwd1 = document.querySelector('.pwd1');
 let pwd2 = document.querySelector('.pwd2');
 let hint = document.querySelector('.hint');
+let correct = document.querySelector('.correct');
 
 function checkPassword() {
     if (pwd1.value != pwd2.value) {
         pwd1.style.borderColor = 'red';
         pwd2.style.borderColor = 'red';
         hint.style.display = 'block';
+        correct.style.display = 'none';
         return false;
     } else {
         pwd1.style.borderColor = 'green';
         pwd2.style.borderColor = 'green';
         hint.style.display = 'none';
+        correct.style.display = 'block';
         return true;
     }
 };
